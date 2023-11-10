6bd8c8e52658e1c9b6c72baac0a8826d1f06533d
jameshuang98 <jameshuang337@gmail.com>
2023-11-09 18:49:32 -0800

    change scroll to top button icon

--- src/containers/topbutton/Top.js
+++ src/containers/topbutton/Top.js
@@ -6,6 +6,7 @@ export default function Top() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
   // When the user scrolls down 20px from the top of the document, show the button
   function scrollFunction() {
     if (
@@ -17,16 +18,21 @@ export default function Top() {
       document.getElementById("topButton").style.visibility = "hidden";
     }
   }
+
+  // Check to make see if button should show or not upon user scrolling
   window.onscroll = function () {
     scrollFunction();
   };
+
+  //To make sure that this button is not visible at starting.
   window.onload = function () {
     scrollFunction();
-  }; //To make sure that this button is not visible at starting.
+  };
+
   // When the user clicks on the button, scroll to the top of the document
   return (
     <button onClick={TopEvent} id="topButton" title="Go to top">
-      <i className="fas fa-hand-point-up" aria-hidden="true"></i>
+      <i className="fas fa-angle-up" aria-hidden="true"></i>
     </button>
   );
 }
